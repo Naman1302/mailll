5fbe4d14e9963a4a07598fb7beab3ff978dec265
Niharika Dhulipala <54968212+Niharikamnit@users.noreply.github.com>
2021-06-26 01:13:15 +0530

    Update login.js

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -5,13 +5,13 @@ const JWT_SECRET = process.env.JWT_SECRET||'gsfi894wt43hiwhfhs8fwb3_efuwbfewf'
 
 
 async function login(req,res){
-    User.findOne({username: req.body.username},async(err,fUser)=>{
+    User.findOne({email: req.body.email},async(err,fUser)=>{
         if(!err && await bcrypt.compare(req.body.password,fUser.password)){
             const token = await jwt.sign({_id:fUser._id},JWT_SECRET)
             res.cookie("ACCESS_TOKEN",token,{
                 httpOnly: true
             });
-            res.json({username: fUser.username})
+            res.json({email: fUser.email, token: token})
         }else{
             console.log(err)
             res.json(false);
